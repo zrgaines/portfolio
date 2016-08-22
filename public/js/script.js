4a7e5ed8e684e2071743f3e7a54c129911490002
Zach Gaines <zrgaines@mtu.edu>
2016-08-21 23:42:42 -0500

    floating nav

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){ 
   $("#typed").typed({
-    strings: ["looking for a job.", "learning React.js", "living in Austin, TX.", ],
-    typeSpeed: 50,
-    startDelay: 10,
-    backSpeed: 50,
+    strings: ["looking for a job.",
+              "learning React.js",
+              "living in Austin, TX.",
+              "enjoying pineapple on his pizza.",
+              "wanting an HTC Vive.",
+              "playing around with three.js.",
+              "making his portfolio better.",
+              "sad that GA is over.",
+              "not missing Michigan weather.",
+              ],
+    typeSpeed: 70,
+    startDelay: 20,
+    backSpeed: 70,
     loop: true,
     loopCount: false
   });
