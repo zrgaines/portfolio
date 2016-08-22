d18d8cdd8c63b7a710325ae232426b1167cf2a92
Zach Gaines <zrgaines@mtu.edu>
2016-08-22 12:28:35 -0500

    added more typed

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){ 
   $("#typed").typed({
-    strings: ["looking for a job.",
+    strings: ["a front-end web developer.",
+              "looking for a job.",
               "learning React.js.",
               "living in Austin, TX.",
+              "thinking about Tokyo 2020.",
+              "sad that GA is over.",
+              "watching South Park.",
               "eating ramen.",
               "wanting an HTC Vive.",
               "playing around with three.js.",
               "making his portfolio better.",
-              "sad that GA is over.",
-              "not missing Michigan weather.",
+              "not missing Michigan weather."
+              
               ],
     typeSpeed: 70,
     startDelay: 20,
