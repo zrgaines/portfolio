0eba70dc7afd161b76356b886ac678ad5e95c2c7
Zach Gaines <zrgaines@mtu.edu>
2016-09-26 13:57:41 -0500

    changed typed sentences

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){ 
   $("#typed").typed({
     strings: ["a front-end web developer.",
-              "looking for a job.",
+              "working at Cylance soon.",
               "learning React.js.",
               "living in Austin, TX.",
               "thinking about Tokyo 2020.",
               "playing around with three.js.",
               "sad that GA is over.",
-              "eating ramen.",
+              "eating sushi.",
               "wanting an HTC Vive.",
               "making his portfolio better.",
               "not missing Michigan weather."
